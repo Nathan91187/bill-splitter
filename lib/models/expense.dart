31c8e43882f2d1a98e999c81df11ac68df9fcfd3
Nathan91187 <natanasnake91@gmail.com>
2026-09-02 15:43:15 +0300

    added expense model

---
+++ lib/models/expense.dart
@@ -0,0 +1,10 @@
+class Expense {
+  final String description;
+  final double price;
+  final int quantity;
+  const Expense({
+    required this.price,
+    required this.description,
+    required this.quantity
+});
+}
