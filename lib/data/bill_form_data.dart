c31daa2ceb8970b7a1aea27fbb8d0368b8569ff8
Nathan91187 <natanasnake91@gmail.com>
2026-09-04 12:17:17 +0300

    added text field manipulation and create bill function

--- lib/data/bill_form_data.dart
+++ lib/data/bill_form_data.dart
@@ -1,18 +1,14 @@
-import 'dart:math';
-
 import 'package:bill_splitter/models/bill.dart';
 import 'package:bill_splitter/models/expense.dart';
-import 'package:bill_splitter/services/auth.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:bill_splitter/models/participant.dart';
 import 'package:flutter/material.dart';
 
 class BillFormData {
-  final uid = FirebaseAuth.instance.currentUser!.uid;
   TextEditingController titleController = TextEditingController();
   List<Map<String, TextEditingController>> expenseFields = [
     {'name': TextEditingController(),
       'price' : TextEditingController(),
-      'amount' : TextEditingController(),
+      'quantity' : TextEditingController(),
     }
   ];
   List <TextEditingController> participants = [TextEditingController()];
@@ -21,26 +17,44 @@ class BillFormData {
           TextEditingController()
       );
   }
+  void removeParticipantField(int index){
+    if(participants.length > 1){
+      participants.removeAt(index);
+    }
+  }
   void addExpenseField(){
       expenseFields.add({
         'name': TextEditingController(),
         'price' : TextEditingController(),
-        'amount' : TextEditingController()
+        'quantity' : TextEditingController()
     });
   }
-  void submitBill(){
+  void removeExpenseField(int index){
+    if(expenseFields.length > 1){
+    expenseFields.removeAt(index);}
+  }
+  Bill createBill(String uid){
     List <Expense> expenses = [];
+    List <Participant> participantList = [];
+    double totalAmount = 0;
     for(final expense in expenseFields){
-      expenses.add(Expense(price: double.parse(expense['price']!.text), description: expense['description']!.text, quantity: int.parse(expense['quantity']!.text)));
+      final price = double.parse(expense['price']!.text);
+      final quantity = int.parse(expense['quantity']!.text);
+      expenses.add(Expense(price: price, description: expense['name']!.text, quantity: quantity));
+      totalAmount +=  price * quantity;
     }
-    final bill = Bill(
-        billID:, createdAt: createdAt, creatorID: uid, expenses: expenses, participants: participants, title: title, totalAmount: totalAmount)
+    for(final participant in participants){
+      participantList.add(Participant(name: participant.text));
+    }
+    return Bill(
+        billID: "not assigned so far", createdAt: DateTime.now(), creatorID: uid, expenses: expenses, participants: participantList, title: titleController.text, totalAmount: totalAmount);
+
   }
   void dispose(){
     for(final expense in expenseFields){
       expense['name']?.dispose();
       expense['price']?.dispose();
-      expense['amount']?.dispose();
+      expense['quantity']?.dispose();
     }
     for(final participant in participants){
       participant.dispose();
