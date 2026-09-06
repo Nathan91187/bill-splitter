45f7e6011f7959a1ef7403ec9a5a36e439ecb131
Nathan91187 <natanasnake91@gmail.com>
2026-09-06 21:01:24 +0300

    moved bill form to screens directory

---
+++ lib/screens/Home/add_bill_form.dart
@@ -0,0 +1,438 @@
+import 'package:bill_splitter/data/bill_form_data.dart';
+import 'package:bill_splitter/models/bill.dart';
+import 'package:bill_splitter/providers/bill_provider.dart';
+import 'package:bill_splitter/shared/common.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class AddBillForm extends StatefulWidget {
+  final Bill? bill;
+
+  const AddBillForm({
+    super.key,
+    this.bill,
+  });
+
+  @override
+  State<AddBillForm> createState() => _AddBillFormState();
+}
+
+class _AddBillFormState extends State<AddBillForm> {
+  final _formKey = GlobalKey<FormState>();
+  final billFormData = BillFormData();
+  final uid = FirebaseAuth.instance.currentUser!.uid;
+
+  @override
+  void initState() {
+    super.initState();
+
+    if (widget.bill != null) {
+      billFormData.fromBill(widget.bill!);
+    }
+  }
+
+  @override
+  void dispose() {
+    billFormData.dispose();
+    super.dispose();
+  }
+
+  void submitBill() {
+    final billProvider = context.read<BillProvider>();
+
+    if (widget.bill == null) {
+      billProvider.addBill(
+        billFormData.createBill(uid),
+      );
+    } else {
+      billProvider.updateBill(
+        billFormData.createBill(uid),
+      );
+    }
+
+    Navigator.pop(context);
+  }
+
+  Widget sectionHeader({
+    required IconData icon,
+    required String title,
+  }) {
+    return Row(
+      children: [
+        Container(
+          padding: const EdgeInsets.all(8),
+          decoration: BoxDecoration(
+            color: Colors.amber.withOpacity(0.12),
+            shape: BoxShape.circle,
+          ),
+          child: Icon(
+            icon,
+            color: Colors.amber,
+            size: 20,
+          ),
+        ),
+        const SizedBox(width: 10),
+        Text(
+          title,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 18,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black87,
+      appBar: AppBar(
+        backgroundColor: Colors.grey,
+        iconTheme: IconThemeData(
+          color: Colors.black
+        ),
+        title: Text(
+          widget.bill == null ? "Add Bill" : "Edit Bill",
+          style: const TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.fromLTRB(12, 20, 12, 30),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                sectionHeader(
+                  icon: Icons.receipt_long_outlined,
+                  title: "Bill Information",
+                ),
+                const SizedBox(height: 12),
+                Container(
+                  margin: const EdgeInsets.only(bottom: 10),
+                  padding: const EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    color: const Color(0xFF111111),
+                    borderRadius: BorderRadius.circular(8),
+                    border: Border.all( color: Colors.amber,),
+                  ),
+                  child: TextFormField(
+                    controller: billFormData.titleController,
+                    validator: (val) {
+                      if (val == null || val.trim().isEmpty) {
+                        return "Enter Bill Name";
+                      }
+                      return null;
+                    },
+                    decoration: textFieldDecoration.copyWith(
+                      hintText: "Bill Name",
+
+                      // prefixIcon: const Icon(
+                      //   Icons.edit_outlined,
+                      //   color: Colors.amber,
+                      // ),
+                    ),
+                  ),
+                ),
+
+                const SizedBox(height: 28),
+                sectionHeader(
+                  icon: Icons.shopping_bag_outlined,
+                  title: "Items",
+                ),
+                const SizedBox(height: 12),
+                ...billFormData.expenseFields.asMap().entries.map(
+                      (entry) {
+                    final index = entry.key;
+                    final expense = entry.value;
+
+                    return Container(
+                      margin: const EdgeInsets.only(bottom: 10),
+                      padding: const EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        color: const Color(0xFF111111),
+                        borderRadius: BorderRadius.circular(8),
+                        border: Border.all( color: Colors.amber,),
+                      ),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Expanded(
+                            flex: 3,
+                            child: TextFormField(
+                              controller: expense['name'],
+                              validator: (val) {
+                                if (val == null ||
+                                    val.trim().isEmpty) {
+                                  return "Enter Item Name";
+                                }
+                                return null;
+                              },
+                              decoration:
+                              textFieldDecoration.copyWith(
+                                hintText: "Item",
+                                // prefixIcon: const Icon(
+                                //   Icons.shopping_bag_outlined,
+                                //   color: Colors.amber,
+                                //   size: 20,
+                                // ),
+                              ),
+                            ),
+                          ),
+
+                          const SizedBox(width: 6),
+
+                          Expanded(
+                            flex: 2,
+                            child: TextFormField(
+                              controller: expense['price'],
+                              keyboardType:
+                              const TextInputType.numberWithOptions(
+                                decimal: true,
+                              ),
+                              validator: (val) {
+                                if (val == null ||
+                                    val.trim().isEmpty) {
+                                  return "Enter Price";
+                                }
+
+                                final price = double.tryParse(val);
+
+                                if (price == null || price <= 0) {
+                                  return "Invalid Price";
+                                }
+
+                                return null;
+                              },
+                              decoration:
+                              textFieldDecoration.copyWith(
+                                hintText: "Price",
+                                // prefixIcon: Icon(
+                                //   Icons.attach_money,
+                                //   color: Colors.amber,
+                                //     size: 20,
+                                // )
+                              ),
+                            ),
+                          ),
+
+                          const SizedBox(width: 6),
+
+                          Expanded(
+                            flex: 2,
+                            child: TextFormField(
+                              controller: expense['quantity'],
+                              keyboardType: TextInputType.number,
+                              validator: (val) {
+                                if (val == null ||
+                                    val.trim().isEmpty) {
+                                  return "Enter Quantity";
+                                }
+
+                                final quantity =
+                                int.tryParse(val);
+
+                                if (quantity == null ||
+                                    quantity <= 0) {
+                                  return "Invalid Quantity";
+                                }
+
+                                return null;
+                              },
+                              decoration:
+                              textFieldDecoration.copyWith(
+                                hintText: "Qty",
+                                // prefixIcon: const Icon(
+                                //   Icons.numbers,
+                                //   color: Colors.amber,
+                                //   size: 20,
+                                // ),
+                              ),
+                            ),
+                          ),
+
+                          if (index != 0)
+                            IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  billFormData
+                                      .removeExpenseField(index);
+                                });
+                              },
+                              icon: const Icon(
+                                Icons.delete_outline,
+                                color: Colors.amber,
+                              ),
+                            ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+
+                const SizedBox(height: 4),
+
+                Center(
+                  child: OutlinedButton.icon(
+                    onPressed: () {
+                      setState(() {
+                        billFormData.addExpenseField();
+                      });
+                    },
+                    icon: const Icon(
+                      Icons.add,
+                      color: Colors.amber,
+                    ),
+                    label: const Text(
+                      "Add Item",
+                      style: TextStyle(
+                        color: Colors.amber,
+                      ),
+                    ),
+                    style: OutlinedButton.styleFrom(
+                      side: const BorderSide(
+                        color: Colors.amber,
+                      ),
+                    ),
+                  ),
+                ),
+
+                const SizedBox(height: 28),
+
+                sectionHeader(
+                  icon: Icons.people_outline,
+                  title: "Participants",
+                ),
+
+                const SizedBox(height: 12),
+
+                ...billFormData.participants.asMap().entries.map(
+                      (entry) {
+                    final index = entry.key;
+                    final participant = entry.value;
+
+                    return Container(
+                      margin: const EdgeInsets.only(bottom: 10),
+                      padding: const EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        color: const Color(0xFF111111),
+                        borderRadius: BorderRadius.circular(8),
+                        border: Border.all( color: Colors.amber,),
+                      ),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: TextFormField(
+                              controller: participant,
+                              validator: (val) {
+                                if (val == null ||
+                                    val.trim().isEmpty) {
+                                  return "Enter Participant Name";
+                                }
+                                return null;
+                              },
+                              decoration:
+                              textFieldDecoration.copyWith(
+                                hintText: "Participant Name",
+                                // prefixIcon: const Icon(
+                                //   Icons.person_outline,
+                                //   color: Colors.amber,
+                                // ),
+                              ),
+                            ),
+                          ),
+
+                          if (index != 0)
+                            IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  billFormData
+                                      .removeParticipantField(index);
+                                });
+                              },
+                              icon: const Icon(
+                                Icons.delete_outline,
+                                color: Colors.amber,
+                              ),
+                            ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+                const SizedBox(height: 4),
+
+                Center(
+                  child: OutlinedButton.icon(
+                    onPressed: () {
+                      setState(() {
+                        billFormData.addParticipantField();
+                      });
+                    },
+                    icon: const Icon(
+                      Icons.person_add_outlined,
+                      color: Colors.amber,
+                    ),
+                    label: const Text(
+                      "Add Participant",
+                      style: TextStyle(
+                        color: Colors.amber,
+                      ),
+                    ),
+                    style: OutlinedButton.styleFrom(
+                      side: const BorderSide(
+                        color: Colors.amber,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 50),
+                SizedBox(
+                  width: double.infinity,
+                  height: 50,
+                  child: FilledButton.icon(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        submitBill();
+                      }
+                    },
+                    icon: Icon(
+                      widget.bill == null
+                          ? Icons.check
+                          : Icons.save_outlined,
+                      color: Colors.black,
+                    ),
+                    label: Text(
+                      widget.bill == null
+                          ? "Create Bill"
+                          : "Save Changes",
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    style: FilledButton.styleFrom(
+                      backgroundColor: Colors.amber,
+                      elevation: 0,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
