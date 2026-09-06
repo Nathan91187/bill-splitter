646a22dc87dfc00f34e6820e3fa530fbf5068bd7
Nathan91187 <natanasnake91@gmail.com>
2026-09-06 21:02:03 +0300

    finalized bill card

--- lib/widgets/bill_card.dart
+++ lib/widgets/bill_card.dart
@@ -1,4 +1,5 @@
 import 'package:bill_splitter/models/bill.dart';
+import 'package:bill_splitter/screens/Home/bill_details.dart';
 import 'package:flutter/material.dart';
 class BillCard extends StatelessWidget {
   final Bill bill;
@@ -11,7 +12,12 @@ class BillCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return InkWell(
       onTap: (){
-        Navigator.pushNamed(context, 'edit_bill',arguments: bill);
+        Navigator.push(context, MaterialPageRoute(
+            builder: (context) => BillDetails(
+                bill: bill
+            )
+        )
+        );
       },
       child: Card(
         color: Colors.black,
@@ -23,74 +29,102 @@ class BillCard extends StatelessWidget {
           ),
         ),
         child: Padding(
-          padding: const EdgeInsets.all(4.0),
+          padding: const EdgeInsets.all(6.0),
           child: Column(
             children: [
               Expanded(
                 flex: 2,
                   child: Container(
                     alignment: Alignment.center,
-                    color: Colors.grey,
+                    decoration: BoxDecoration(
+                        color: Colors.grey,
+                        borderRadius: BorderRadius.vertical(
+                          top: Radius.circular(3)
+                        )
+                    ),
                     child: Padding(
                       padding: const EdgeInsets.symmetric(horizontal: 4.0),
                       child: Text(
-                        bill.title
+                        bill.title,
+                        textAlign: TextAlign.center,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600
+                        ),
                       ),
                     ),
                   )),
-              SizedBox(height: 20),
+              SizedBox(height: 12),
               Expanded(
-                flex: 4,
-                  child: Row(
-                    children: [
-                      Text(
-                        "\$${bill.totalAmount.toString()}",
-                        style: TextStyle(
-                          color: Colors.amber,
-                          fontSize: 30
-                        ),
+                flex: 3,
+                  child: Center(
+                    child: Text(
+                      "\$${bill.totalAmount.toStringAsFixed(2)}",
+                      style: TextStyle(
+                        color: Colors.amber,
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold
                       ),
-                    ],
+                    ),
                   ),
               ),
-              SizedBox(height: 20),
+              SizedBox(height: 12),
               Expanded(
                   flex: 5,
                   child: Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 6),
                     alignment: Alignment.center,
-                    color: Colors.grey,
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 4),
-                      child: Column(
-                        children: [
-                          Row(
-                            children: [
-                              Icon(
-                                Icons.people,
-                                color: Colors.amber,
-                                fontWeight: FontWeight.bold,
+                    decoration: BoxDecoration(
+                      color: Colors.grey,
+                      borderRadius: BorderRadius.vertical(
+                        bottom: Radius.circular(3)
+                      )
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.people,
+                              color: Colors.amber,
+                              size: 20,
+                            ),
+                            SizedBox(width: 6),
+                            Text(
+                                "${bill.participants.length} Participants",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w500
                               ),
-                              SizedBox(width: 4),
-                              Text(
-                                  "${bill.participants.length} Participants"
-                              ),
-                            ],
+                            ),
+                          ],
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.symmetric(
+                              vertical: 3
                           ),
-                          Container(height: 1,color: Colors.black),
-                          Row(
-                            children: [
-                              Icon(
-                                Icons.calendar_month,
-                                color: Colors.amber,
-                              ),
-                              SizedBox(width: 4),
-                              Text(
-                                  "${bill.createdAt.year}/${bill.createdAt.month}/${bill.createdAt.day}"
-                              ),
-                            ],
+                          child: Divider(
+                            color: Colors.black,
+                            height: 1,
                           ),
-                        ],
-                      ),
+                        ),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.calendar_month,
+                              color: Colors.amber,
+                            ),
+                            SizedBox(width: 4),
+                            Text(
+                                "${bill.createdAt.year}/${bill.createdAt.month}/${bill.createdAt.day}",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w600
+                                ),
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
                   )),
             ],
