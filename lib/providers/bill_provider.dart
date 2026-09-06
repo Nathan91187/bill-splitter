e65e7066921786c16a6fd537f0c07dae5d267be9
Nathan91187 <natanasnake91@gmail.com>
2026-09-06 21:03:30 +0300

    added update bill method

--- lib/providers/bill_provider.dart
+++ lib/providers/bill_provider.dart
@@ -1,11 +1,45 @@
 import 'package:bill_splitter/models/bill.dart';
 import 'package:bill_splitter/models/participant.dart';
+import 'package:bill_splitter/services/auth.dart';
 import 'package:flutter/material.dart';
 
+import '../models/expense.dart';
+
 class BillProvider extends ChangeNotifier{
-  List <Bill> billList = [];
+  final uid = AuthService().auth.currentUser!.uid;
+  List <Expense> expenses = [
+    Expense(price: 30, description: "x", quantity: 3),
+    Expense(price: 20, description: "y", quantity: 4),
+    Expense(price: 40, description: "z", quantity: 4) ];
+  List <Participant> participants = [
+    Participant(name: "nate"),
+    Participant(name: "Nathan"),
+    Participant(name: "Trobbio") ];
+  List <Bill> billList = [
+    Bill(billID: "To be Specfied",
+  createdAt: DateTime.now(),
+  creatorID: "uid",
+  expenses:[
+  Expense(price: 30, description: "x", quantity: 3),
+  Expense(price: 20, description: "y", quantity: 4),
+  Expense(price: 40, description: "z", quantity: 4)],
+  participants: [
+  Participant(name: "nate"),
+  Participant(name: "Nathan"),
+  Participant(name: "Trobbio"),
+  ],
+  title: "Lunch",
+  totalAmount: 120)];
+
   void addBill(Bill bill){
     billList.add(bill);
+    notifyListeners();
+  }
+  void updateBill(Bill bill){
+    final index = billList.indexWhere((currentBill) => bill.billID == currentBill.billID);
+    if(index != -1){
+      billList[index] = bill;
+    }
     notifyListeners();
   }
   void removeBill(String billID){
