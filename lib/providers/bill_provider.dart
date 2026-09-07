25cbc17d50a643add72274784edab96a135747d7
Nathan91187 <natanasnake91@gmail.com>
2026-09-07 21:29:31 +0300

    included persistence to adding editing and removing bill

--- lib/providers/bill_provider.dart
+++ lib/providers/bill_provider.dart
@@ -1,55 +1,33 @@
+import 'dart:async';
+
 import 'package:bill_splitter/models/bill.dart';
 import 'package:bill_splitter/models/participant.dart';
 import 'package:bill_splitter/services/auth.dart';
+import 'package:bill_splitter/services/bill_service.dart';
 import 'package:flutter/material.dart';
-
-import '../models/expense.dart';
-
 class BillProvider extends ChangeNotifier{
-  final uid = AuthService().auth.currentUser!.uid;
-  List <Expense> expenses = [
-    Expense(price: 30, description: "x", quantity: 3),
-    Expense(price: 20, description: "y", quantity: 4),
-    Expense(price: 40, description: "z", quantity: 4) ];
-  List <Participant> participants = [
-    Participant(name: "nate"),
-    Participant(name: "Nathan"),
-    Participant(name: "Trobbio") ];
-  List <Bill> billList = [
-    Bill(billID: "To be Specfied",
-  createdAt: DateTime.now(),
-  creatorID: "uid",
-  expenses:[
-  Expense(price: 30, description: "x", quantity: 3),
-  Expense(price: 20, description: "y", quantity: 4),
-  Expense(price: 40, description: "z", quantity: 4)],
-  participants: [
-  Participant(name: "nate"),
-  Participant(name: "Nathan"),
-  Participant(name: "Trobbio"),
-  ],
-  title: "Lunch",
-  totalAmount: 120)];
+  final billService = BillService();
+  List <Bill> billList = [];
+  StreamSubscription<List<Bill>>? _billSubscription;
+  BillProvider(){
 
-  void addBill(Bill bill){
-    billList.add(bill);
-    notifyListeners();
+    _billSubscription = billService.bills.listen((bills){
+      billList = bills;
+      notifyListeners();
+    });
   }
-  void updateBill(Bill bill){
-    final index = billList.indexWhere((currentBill) => bill.billID == currentBill.billID);
-    if(index != -1){
-      billList[index] = bill;
-    }
-    notifyListeners();
+  @override
+  void dispose(){
+    _billSubscription?.cancel();
+    super.dispose();
+
   }
-  void removeBill(String billID){
-    Bill billToBeRemoved = billList.firstWhere((val) => val.billID == billID);
-    billList.remove(billToBeRemoved);
-    notifyListeners();
+
+
+  Future <void> addBill(Bill bill) async {
+    return await billService.saveBill(bill);
   }
-  void removeParticipant(Participant participant, String billID){
-    Bill thisBill = billList.firstWhere((val) => val.billID == billID);
-    thisBill.participants.remove(participant);
-    notifyListeners();
+  Future<void> removeBill(String billID) async{
+   return await billService.removeBill(billID);
   }
 }
