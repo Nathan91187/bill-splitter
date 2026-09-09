fd3b9fc3ed441752df538bf4e8a1b75525b344e7
Nathan91187 <natanasnake91@gmail.com>
2026-09-09 21:32:35 +0300

    added group provider

---
+++ lib/providers/group_provider.dart
@@ -0,0 +1,29 @@
+import 'dart:async';
+
+import 'package:bill_splitter/models/bill_group.dart';
+import 'package:bill_splitter/services/group_service.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
+
+class GroupProvider extends ChangeNotifier{
+  List<BillGroup> groupList = [];
+  GroupService groupService = GroupService();
+  StreamSubscription<List<BillGroup>>? _groupSubscription;
+  GroupProvider(){
+    _groupSubscription = GroupService().groups.listen((groups){
+      groupList = groups;
+      notifyListeners();
+    });
+  }
+  void dispose(){
+    super.dispose();
+    _groupSubscription?.cancel();
+  }
+  Future<void> addGroup (BillGroup group) async{
+    await groupService.saveGroup(group);
+  }
+  Future<void> removeGroup(String groupID) async{
+    await groupService.removeGroup(groupID);
+  }
+
+}
