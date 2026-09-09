c68296e13a583ce0381a8846322096d5c3c9bab6
Nathan91187 <natanasnake91@gmail.com>
2026-09-09 21:32:15 +0300

    added group-list base widget

---
+++ lib/widgets/group_list.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+class GroupList extends StatelessWidget {
+  const GroupList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Your Groups"),
+    );
+  }
+}
