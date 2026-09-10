aecf77a79db2a76cb870931f633dd482d00c797e
Nathan91187 <natanasnake91@gmail.com>
2026-09-10 21:56:21 +0300

    modified group list widget

--- lib/widgets/group_list.dart
+++ lib/widgets/group_list.dart
@@ -1,11 +1,28 @@
+import 'package:bill_splitter/providers/group_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 class GroupList extends StatelessWidget {
   const GroupList({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Your Groups"),
-    );
+    final groupList = Provider.of<GroupProvider>(context).groupList;
+    return groupList.isEmpty ? Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          Icons.groups_outlined,
+          color: Colors.grey.withOpacity(0.25),
+          size: 60,
+        ),
+        Text(
+          "No Groups Yet",
+          style: TextStyle(
+              color: Colors.grey.withOpacity(0.25),
+              fontSize: 20
+          ),
+        )
+      ],
+    ) : ,
   }
 }
