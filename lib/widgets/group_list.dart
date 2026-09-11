12cc4c3562efd04ee6a4724755d2dd58946e9401
Nathan91187 <natanasnake91@gmail.com>
2026-09-12 00:07:55 +0300

    populated group list with actual groups

--- lib/widgets/group_list.dart
+++ lib/widgets/group_list.dart
@@ -1,4 +1,5 @@
 import 'package:bill_splitter/providers/group_provider.dart';
+import 'package:bill_splitter/widgets/group_card.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 class GroupList extends StatelessWidget {
@@ -7,22 +8,30 @@ class GroupList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final groupList = Provider.of<GroupProvider>(context).groupList;
-    return groupList.isEmpty ? Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(
-          Icons.groups_outlined,
-          color: Colors.grey.withOpacity(0.25),
-          size: 60,
-        ),
-        Text(
-          "No Groups Yet",
-          style: TextStyle(
-              color: Colors.grey.withOpacity(0.25),
-              fontSize: 20
+    return groupList.isEmpty ? Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            Icons.groups_outlined,
+            color: Colors.grey.withOpacity(0.25),
+            size: 60,
           ),
-        )
-      ],
-    ) : ,
+          SizedBox(width: 10,),
+          Text(
+            "No Groups Yet",
+            style: TextStyle(
+                color: Colors.grey.withOpacity(0.25),
+                fontSize: 20
+            ),
+          )
+        ],
+      ),
+    ) : ListView.builder(
+        itemBuilder: (context,index) {
+          return GroupCard(billGroup: groupList[index]);
+        },
+        itemCount: groupList.length,
+    );
   }
 }
