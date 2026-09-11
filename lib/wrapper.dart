f3c16d067fd17e600fc39907476ded231b58fbd1
Nathan91187 <natanasnake91@gmail.com>
2026-09-12 00:04:09 +0300

    modified wrapper to ask username for new users

--- lib/wrapper.dart
+++ lib/wrapper.dart
@@ -1,9 +1,14 @@
 import 'package:bill_splitter/models/user.dart';
 import 'package:bill_splitter/providers/bill_provider.dart';
 import 'package:bill_splitter/providers/group_provider.dart';
-import 'package:bill_splitter/screens/Home/Home.dart';
+import 'package:bill_splitter/screens/Home.dart';
+import 'package:bill_splitter/screens/groups/groups.dart';
+import 'package:bill_splitter/screens/authentication/display_name_form.dart';
 import 'package:bill_splitter/screens/authentication/authenticate.dart';
 import 'package:bill_splitter/services/bill_service.dart';
+import 'package:bill_splitter/services/user_service.dart';
+import 'package:bill_splitter/shared/loading.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -14,21 +19,33 @@ class Wrapper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final user = Provider.of<UserModel?>(context);
+    final user = Provider.of<User?>(context);
     if(user == null){
       return Authenticate();
     }
-    return MultiProvider(
+    return FutureBuilder<UserModel?>(
+      future:  UserService().findUserById(FirebaseAuth.instance.currentUser!.uid),
+      builder: (context,snapshot){
+        if(snapshot.connectionState == ConnectionState.waiting){
+          return Loading();
+        }
+        final namedUser = snapshot.data;
+        if(namedUser == null){
+          return const DisplayNameForm();
+        }
+        return MultiProvider(
             providers: [
               ChangeNotifierProvider(
                 create: (_) => BillProvider(),
 
-                ),
+              ),
               ChangeNotifierProvider(
                   create: (_) => GroupProvider())
             ],
-                child: MaterialApp(
-                home: const Home(),
-          ));
+            child: const MaterialApp(
+              home: Home(),
+            ));
+      },
+    );
   }
 }
