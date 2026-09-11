bed44737a022057a821ec0d85f702bef9b122fd6
Nathan91187 <natanasnake91@gmail.com>
2026-09-12 00:04:37 +0300

    added user name and email to our user model

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,7 +1,10 @@
 class UserModel {
   final String uid;
-
+  final String displayName;
+  final String email;
   const UserModel({
-    required this.uid
+    required this.uid,
+    required this.displayName,
+    required this.email
   });
 }
