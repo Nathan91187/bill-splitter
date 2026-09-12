0e394260f4e790b056e98bdfc1f2db278e41ef46
Nathan91187 <natanasnake91@gmail.com>
2026-09-12 22:49:06 +0300

    renamed auth errors

--- lib/data/auth_errors.dart
+++
@@ -1,9 +0,0 @@
-class AuthErrors {
-  Map<String,String> authErrorMessages = {
-    'network-request-failed' : 'No internet connection. Please connect to the internet and try again.',
-  'invalid-email' : 'Please enter a valid email address.',
-  'invalid-credential' : 'Invalid email or password.',
-  'email-already-in-use' : 'An account with this email already exists',
-    'weak-password' : 'The password is too weak'
-  };
-}
