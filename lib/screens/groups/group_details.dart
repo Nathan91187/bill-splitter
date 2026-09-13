68143cbe236f3133669323f014f95ec8a61b2ab4
Nathan91187 <natanasnake91@gmail.com>
2026-09-13 21:39:15 +0300

    included pop up menu items with their implementation

--- lib/screens/groups/group_details.dart
+++ lib/screens/groups/group_details.dart
@@ -1,7 +1,9 @@
 import 'package:bill_splitter/models/bill_group.dart';
 import 'package:bill_splitter/models/user.dart';
 import 'package:bill_splitter/providers/group_provider.dart';
-import 'package:bill_splitter/screens/invite_form.dart';
+import 'package:bill_splitter/screens/add_bill_form.dart';
+import 'package:bill_splitter/screens/groups/create_group_form.dart';
+import 'package:bill_splitter/screens/invitation/invite_form.dart';
 import 'package:bill_splitter/services/group_service.dart';
 import 'package:bill_splitter/services/user_service.dart';
 import 'package:bill_splitter/shared/loading.dart';
@@ -11,26 +13,72 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class GroupDetails extends StatelessWidget {
-  final BillGroup billGroup;
+class GroupDetails extends StatefulWidget {
+  final String groupID;
   const GroupDetails({
     super.key,
-    required this.billGroup
+    required this.groupID
   });
 
+  @override
+  State<GroupDetails> createState() => _GroupDetailsState();
+}
 
+class _GroupDetailsState extends State<GroupDetails> {
+  bool loading = false;
+  final userService = UserService();
+  final currUser = FirebaseAuth.instance.currentUser!;
   @override
   Widget build(BuildContext context) {
-    final userService = UserService();
-    final currUser = FirebaseAuth.instance.currentUser!;
+
     final groupProvider = Provider.of<GroupProvider>(context);
+    final billGroup = groupProvider.getGroupById(widget.groupID);
     Future<List<UserModel>> getUsers() async{
       final users = await Future.wait(
-        billGroup.memberIDs.map((memberId)=> userService.findUserById(memberId))
+        billGroup!.memberIDs.map((memberId)=> userService.findUserById(memberId))
       );
       return users.whereType<UserModel>().toList();
     }
-    return Scaffold(
+    if (billGroup == null) {
+      return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.grey,
+          actions: [
+          ],
+          leading: IconButton(
+              onPressed: (){
+                Navigator.pop(context);
+              },
+              icon: Icon(Icons.arrow_back)),
+        ),
+        backgroundColor: Colors.black,
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+               const Icon(
+                  Icons.group_off_outlined,
+                  color: Colors.grey,
+                  size: 70,
+                ),
+                const SizedBox(height: 16,),
+                const Text(
+                  textAlign: TextAlign.center,
+                  "You no longer have access to this group",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      );
+    }
+    return loading ? Loading() :Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
         backgroundColor: Colors.grey,
@@ -45,7 +93,30 @@ class GroupDetails extends StatelessWidget {
               color: Colors.black,
               onSelected: (value){
                 if(value == 'add_bill'){
-
+                  Navigator.push(context, MaterialPageRoute(
+                      builder: (context)=> AddBillForm(groupID: widget.groupID,)));
+                }
+                else if(value == 'edit_group'){
+                      Navigator.push(context, MaterialPageRoute(
+                          builder: (context)=> CreateGroupForm(billGroup: billGroup)));
+                }
+                else if(value == 'delete_group'){
+                  ConfirmationDialog().showConfirmationDialog(context,
+                      "Delete Group?",
+                      "Are you sure you want to delete this group."
+                          "This action can't be undone",
+                      "Cancel",
+                      "Delete",
+                      () async {
+                    setState(() {
+                      loading = true;
+                    });
+                        await groupProvider.removeGroup(widget.groupID);
+                        if(context.mounted){
+                          Navigator.pop(context);
+                        }
+                      },
+                      Icons.delete_outline);
                 }
                 else if(value == 'invite_users'){
                   Navigator.push(context, MaterialPageRoute(
@@ -54,6 +125,29 @@ class GroupDetails extends StatelessWidget {
                 }
               },
             itemBuilder: (context) =>[
+              if(currUser.uid == billGroup.creatorID)
+              PopupMenuItem(
+                value: 'edit_group',
+                child: Container(
+                  color: Colors.black,
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.edit_outlined,
+                        color: Colors.amber,
+                      ),
+                      SizedBox(width: 10),
+                      Text(
+                        'Edit Group',
+                        style: TextStyle(
+                            color: Colors.amber
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
                  PopupMenuItem(
                    value: 'add_bill',
                    child: Container(
@@ -92,8 +186,31 @@ class GroupDetails extends StatelessWidget {
                   ],
                 ),
               ),
+              if(currUser.uid == billGroup.creatorID)
+              PopupMenuItem(
+                value: 'delete_group',
+                child: Container(
+                  color: Colors.black,
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.delete_outline,
+                        color: Colors.amber,
+                      ),
+                      SizedBox(width: 10),
+                      Text(
+                        'Delete Group',
+                        style: TextStyle(
+                            color: Colors.amber
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ],
           )
+
         ],
       ),
       body: SingleChildScrollView(
@@ -145,12 +262,12 @@ class GroupDetails extends StatelessWidget {
                           leading: Container(
                             padding: const EdgeInsets.all(7),
                             decoration: BoxDecoration(
-                              color: Colors.amber.withOpacity(0.12),
+                              color: Colors.grey.withOpacity(0.12),
                               shape: BoxShape.circle,
                             ),
                             child: const Icon(
                               Icons.person_outline,
-                              color: Colors.amber,
+                              color: Colors.grey,
                               size: 20,
                             ),
                           ),
@@ -189,16 +306,32 @@ class GroupDetails extends StatelessWidget {
                                       "Remove",
                                           () async {
                                         await groupProvider.removeMember(
-                                          eachMember.uid,
                                           billGroup.groupID!,
+                                          eachMember.uid,
                                         );
                                       },
                                       Icons.person_remove_outlined,
                                     ),
                                     icon: Icon(
                                       Icons.delete_outline_outlined,
-                                      color: Colors.amber,
+                                      color: Colors.grey,
                                     )),
+                              if(eachMember.uid == currUser.uid && eachMember.uid != billGroup.creatorID)
+                                Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(4),
+                                    color: Colors.grey.withOpacity(0.12),
+                                  ),
+
+                                  padding: EdgeInsets.symmetric(horizontal: 3, vertical: 1),
+                                  child: Text(
+                                    'You',
+                                    style: TextStyle(
+                                      color: Colors.grey,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                ),
                               if(eachMember.uid == billGroup.creatorID)
                                 Container(
                                   decoration: BoxDecoration(
