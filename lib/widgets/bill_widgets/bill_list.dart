13ea48745f5c4c2da208c17d620ac32049167f0c
Nathan91187 <natanasnake91@gmail.com>
2026-09-14 23:06:03 +0300

    initialized provider at run time based on group or standalone bill and returned a consumer widget

--- lib/widgets/bill_widgets/bill_list.dart
+++ lib/widgets/bill_widgets/bill_list.dart
@@ -1,40 +1,61 @@
+import 'package:bill_splitter/models/bill.dart';
 import 'package:bill_splitter/providers/bill_provider.dart';
 import 'package:bill_splitter/widgets/bill_widgets/bill_card.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class BillList extends StatelessWidget {
-  const BillList({super.key});
+class BillList extends StatefulWidget {
+  final String? groupId;
+  const BillList({
+    this.groupId,
+    super.key});
 
   @override
+  State<BillList> createState() => _BillListState();
+}
+class _BillListState extends State<BillList> {
+  @override
+  void initState(){
+    super.initState();
+    if(widget.groupId == null){
+      context.read<BillProvider>().listenStandaloneBills();
+    }
+    else{
+      context.read<BillProvider>().listenGroupBills(widget.groupId!);
+    }
+  }
+  @override
   Widget build(BuildContext context) {
-    final billList = Provider.of<BillProvider>(context).billList;
-    return billList.isEmpty? Center(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(
-            Icons.receipt_long_outlined,
-            color: Colors.grey.withOpacity(0.25),
-            size: 60,
-          ),
-          SizedBox(width: 10,),
-          Text(
-            "No Bills to Show",
-            style: TextStyle(
+
+    return Consumer<BillProvider>(
+        builder: (context,billProvider,child) => billProvider.billList.isEmpty? Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.receipt_long_outlined,
                 color: Colors.grey.withOpacity(0.25),
-                fontSize: 20
+                size: 60,
+              ),
+              SizedBox(width: 10,),
+              Text(
+                "No Bills to Show",
+                style: TextStyle(
+                    color: Colors.grey.withOpacity(0.25),
+                    fontSize: 20
+                ),
+              )
+            ],
+          ),
+        ) : GridView.builder(
+          shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2
             ),
-          )
-        ],
-      ),
-    ) : GridView.builder(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2
-        ),
-        itemCount: billList.length,
-        itemBuilder: (context,index){
-          return BillCard(bill: billList[index]);
-        });
+            itemCount: billProvider.billList.length,
+            itemBuilder: (context,index){
+              return BillCard(bill: billProvider.billList[index]);
+            }));
   }
 }
