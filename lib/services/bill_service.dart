e1f0da4d0f8822030b917a31ee14be0449f2c4dd
Nathan91187 <natanasnake91@gmail.com>
2026-09-14 23:08:14 +0300

    included a group bill stream based on group id

--- lib/services/bill_service.dart
+++ lib/services/bill_service.dart
@@ -1,5 +1,6 @@
 import 'package:bill_splitter/models/expense.dart';
 import 'package:bill_splitter/models/participant.dart';
+import 'package:bill_splitter/services/group_service.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import '../models/bill.dart';
@@ -7,15 +8,19 @@ import '../models/bill.dart';
 class BillService {
   final billCollection = FirebaseFirestore.instance.collection("bills");
   final uid = FirebaseAuth.instance.currentUser!.uid;
+  String? groupId;
   Future <void> saveBill(Bill bill) async {
     final docRef = bill.billID.isEmpty ?  billCollection.doc() : billCollection.doc(bill.billID);
    await docRef.set({
+     'is_group' : bill.groupID != null,
+      'group_id' : bill.groupID,
       'title' : bill.title,
       'creator_id' : bill.creatorID,
       'participants' : bill.participants.map((participant){
         return {
           'name' : participant.name,
-          'has_paid' : participant.hasPaid
+          'has_paid' : participant.hasPaid,
+          'uid': participant.uid
         };
       }).toList(),
       'expenses' : bill.expenses.map((expense){
@@ -37,6 +42,7 @@ class BillService {
     return snapshot.docs.map((doc){
       return Bill(
           billID: doc.id,
+          groupID: doc['group_id'],
           createdAt: (doc['date'] as Timestamp).toDate(),
           creatorID: doc['creator_id'],
           expenses: (doc['expenses'] as List).map((expense) {
@@ -48,7 +54,8 @@ class BillService {
           participants: (doc['participants'] as List).map((participant){
             return Participant(
                 name: participant['name'],
-                hasPaid: participant['has_paid']
+                hasPaid: participant['has_paid'],
+                uid: participant['uid']
             );
           }).toList(),
           title: doc['title'],
@@ -58,7 +65,12 @@ class BillService {
     }).toList();
   }
   Stream <List<Bill>> get bills{
-    return billCollection.where('creator_id' , isEqualTo: uid).snapshots().map(_billListFromSnapshot);
-
+    return billCollection.where('is_group', isEqualTo: false).where('creator_id' , isEqualTo: uid).snapshots().map(_billListFromSnapshot);
+  }
+  Stream<List<Bill>> getGroupBills(String groupID){
+    return billCollection
+        .where('is_group' , isEqualTo: true)
+        .where('group_id' , isEqualTo: groupID)
+        .snapshots().map(_billListFromSnapshot);
   }
   }
